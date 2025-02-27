5b7209f2d4780148006990eb1c942fb11355fbb0
Hasret Deniz Bulut <hbulut@k1m36s03.42istanbul.com.tr>
2025-02-27 19:41:08 +0300

    add_steps

--- push_swap_r/steps/push.c
+++ push_swap_r/steps/push.c
@@ -6,7 +6,7 @@
 /*   By: hbulut <hbulut@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 21:08:39 by hbulut            #+#    #+#             */
-/*   Updated: 2025/02/25 20:07:05 by hbulut           ###   ########.fr       */
+/*   Updated: 2025/02/27 18:36:02 by hbulut           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,88 @@
 // pb (push b): a stackinin en üstündeki elemanı b stackinin en üstüne koyun.
 // Eğer a stacki boş ise bir şey yapmayın.
 
-#include "push_swap.h"
+//#include "push_swap.h"
 
-void push_a(t_list a, t_list b)
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct s_list	t_list;
+
+typedef struct s_list
 {
-	
+	int					value;
+	t_list				*next;
+
+}						t_list;
+
+void	push_a(t_list **a, t_list **b)
+{
+	t_list	*tmp;
+
+	if (b == NULL || (*b) == NULL || a == NULL)
+		return ;
+	tmp = *b;
+	*b = tmp->next;
+	tmp->next = *a;
+	*a = tmp;
+	write(1, "pa\n", 3);
 }
 
-void push_b(t_list a, t_list b)
+void	push_b(t_list **a, t_list **b)
 {
-	
+	t_list	*tmp;
+
+	if (b == NULL || a == NULL || (*a) == NULL)
+		return ;
+	tmp = *a;
+	*a = tmp->next;
+	tmp->next = *b;
+	*b = tmp;
+	write(1, "pb\n", 3);
+}
+
+void	add_new(t_list **stack, int value)
+{
+	t_list	*new_node;
+
+	new_node = malloc(sizeof(t_list));
+	if (!new_node)
+		return ;
+	new_node->value = value;
+	new_node->next = *stack;
+	*stack = new_node;
+}
+
+void	print_stack(t_list *stack)
+{
+	while (stack)
+	{
+		printf("%d\n", stack->value);
+		stack = stack->next;
+	}
+	printf("NULL*\n");
+}
+
+int	main(int argc, char **argv)
+{
+	t_list	*a;
+	t_list	*b;
+
+	a = NULL;
+	b = NULL;
+	add_new(&a, 3);
+	add_new(&a, 2);
+	add_new(&a, 1);
+	add_new(&b, 4);
+	add_new(&b, 5);
+
+	print_stack(a);
+	print_stack(b);
+
+	push_a(&a, &b);
+
+	print_stack(a);
+	print_stack(b);
+	return (0);
 }
--- push_swap_r/steps/reverse_rotate.c
+++ push_swap_r/steps/reverse_rotate.c
@@ -6,7 +6,7 @@
 /*   By: hbulut <hbulut@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 21:08:41 by hbulut            #+#    #+#             */
-/*   Updated: 2025/02/20 19:47:35 by hbulut           ###   ########.fr       */
+/*   Updated: 2025/02/27 19:36:50 by hbulut           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,3 +17,40 @@
 // Son eleman artık ilk eleman olacaktır.
 
 // rrr : rra ve rrb aksiyonlarının ikisini de çalıştırır
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct s_list	t_list;
+
+typedef struct s_list
+{
+	int					value;
+	t_list				*next;
+
+}						t_list;
+
+void	reverse_rotate_a(t_list **a)
+{
+	t_list *tmp;
+	int i = 0;
+
+	if (a == NULL || (*a) == NULL || (*a)->next == NULL)
+		return ;
+	tmp = *a;
+	while (tmp->next->next != NULL)
+	{
+		i++;
+		tmp = tmp->next;
+	}
+	if (i == 1)
+		return ;
+	tmp->next = NULL;
+	
+	
+	
+}
+
+
+
